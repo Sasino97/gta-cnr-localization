0e1113fa8ab6e867983f1322c3b0c84db1ebc313
Salvatore Strazzullo <sasino1997@gmail.com>
2024-11-18 12:10:51 +0700

    Added fxmanifest as this is now its own resource

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sal "Sasino" Strazzullo, Sasinosoft Games, Strazzullo.NET LLC'
+description 'GTA CnR Localization'
+version '0.3'
+
+files {
+    'data/**/*.xml'
+}
