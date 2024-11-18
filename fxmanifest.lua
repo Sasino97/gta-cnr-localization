db52405e3093c40d8eb73a491f216c12736fbe50
Salvatore Strazzullo <sasino1997@gmail.com>
2024-11-18 13:08:53 +0700

    Updated all manifest files with up to date copyright information and correct authors/contributors

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Sal "Sasino" Strazzullo, Sasinosoft Games, Strazzullo.NET LLC'
-description 'GTA CnR Localization'
+description 'GTA CnR Localization Entries'
 version '0.3'
+author 'CnR Translators Team and all the contributors at https://github.com/Sasino97/gta-cnr-localization'
+copyright 'Sasinosoft Games, Strazzullo Software LLC'
 
 files {
     'index.json',
